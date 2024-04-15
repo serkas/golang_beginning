08d5270d3412e0396253b204208a29241d660894
Serhii Kashuba <serhii.kashuba@solidgate.com>
2024-04-15 10:33:30 +0300

    14. gomod starter

---
+++ 14_teamwork/lesson/gomod/go.mod
@@ -0,0 +1,3 @@
+module gomod-demo
+
+go 1.22.1
---
+++ 14_teamwork/lesson/gomod/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/serhiikas9/strtools"
+)
+
+func main() {
+	// https://github.com/serhiikas9/strtools/tags
+	str := "qwerty"
+
+	fmt.Println(str)
+	//fmt.Println(strtools.Reverse(str))
+}
